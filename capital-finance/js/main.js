53265014819e1eeab4d0e7aca298973571203acf
pavelevriksavenkov <33938705+pavelevriksavenkov@users.noreply.github.com>
2020-03-18 07:44:41 +0500

    создал капитал финанс, about.html, файлы стилей и js

---
+++ capital-finance/js/main.js
@@ -0,0 +1,8 @@
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    loop: true,
+    navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
+    },
+});
