d56c38874f78ab566f865896b78559c4d0527690
Andrey Kolchanov <eandy@inbox.ru>
2020-04-04 17:53:28 +0500

    Обновление

---
+++ repair-boilers/js/main_slider.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel(
+
+        {
+            loop:true,
+            margin:0,
+            nav:false,
+            navText : ["",""],
+            dots:false,
+            items: 1,
+            autoplay:false,
+            autoplayTimeout:3000,
+            autoplayHoverPause:true
+        }
+
+    );
+});
