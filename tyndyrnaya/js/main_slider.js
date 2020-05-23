d668d4d4feffcc635361c47ec11f9b82979bf996
Andrey Kolchanov <eandy@inbox.ru>
2020-05-23 22:12:04 +0500

    Обновление

--- tyndyrnaya/js/main_slider.js
+++ tyndyrnaya/js/main_slider.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
     $("#owl-carousel1").owlCarousel(
         {
-            loop:true,
-            margin:0,
+            loop: true,
+            margin: 0,
             items: 1,
             dots: false,
             nav: true,
-            autoplay: false,
-            autoplayTimeout:6000,
-            autoplayHoverPause:true
+            autoplay: true,
+            autoplayTimeout: 6000,
+            autoplayHoverPause: true
         }
     );
     $("#owl-carousel2").owlCarousel(
         {
             loop:true,
-            margin:0,
+            margin: 0,
             items: 1,
             dots: false,
-            nav:true,
-            navText : ["<",">"],
+            nav: true,
+            navText: ["<",">"],
         }
     );
 });
