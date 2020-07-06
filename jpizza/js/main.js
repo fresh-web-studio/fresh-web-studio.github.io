e3f58923c677cc6f79083529d32a8848ac5c0579
Andrey Kolchanov <eandy@inbox.ru>
2020-07-06 23:47:56 +0500

    Обновление

--- jpizza/js/main.js
+++
@@ -1,8 +0,0 @@
-var mySwiper = new Swiper('.banner-swiper', {
-    slidesPerView: 1,
-    loop: true,
-    navigation: {
-        nextEl: '.banner-right-arrow',
-        prevEl: '.banner-left-arrow',
-    },
-});
