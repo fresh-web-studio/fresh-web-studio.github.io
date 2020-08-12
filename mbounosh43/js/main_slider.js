8580cce3d4b8e681b5167381961f7a17af86c9e6
Andrey Kolchanov <eandy@inbox,.ru>
2020-08-12 19:19:56 +0500

    Creating a header and slider in the PC version

---
+++ mbounosh43/js/main_slider.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#owl-carousel1").owlCarousel(
+        {
+            loop:true,
+            items: 1,
+            nav:true,
+            navText: ['&#xf053;','&#xf054;'],
+            autoplay:true,
+            autoplayTimeout:6000,
+            autoplayHoverPause:true,
+        }
+    )
+})
